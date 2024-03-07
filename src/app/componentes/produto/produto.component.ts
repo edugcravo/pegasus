b2df2bebd17db91aa72268d3a7b80c7792f867e1
edugcravo <edugcravo@hotmail.com>
2024-03-07 04:13:10 -0300

    Atualizações de estilo e funcionalidade para o componente de produto

--- src/app/componentes/produto/produto.component.ts
+++ src/app/componentes/produto/produto.component.ts
@@ -50,6 +50,7 @@ export class ProdutoComponent implements OnInit {
     this.produtoService.getProdutoId(localStorage.getItem('idProduto')).subscribe((res: any) => {
       console.log(res)
       this.produto = res.produto
+      this.produtosRelacionados(this.produto.categoria)
       this.imagemPrincipal = this._sanitizer.bypassSecurityTrustResourceUrl('data:image/jpg;base64,' + this.produto.imagens[0]);
       // Sanitizar imagens
       this.produto.imagens.forEach((element: any, index: number) => {
@@ -63,4 +64,54 @@ export class ProdutoComponent implements OnInit {
   mudarFotoPrincipal(imagem: any){
     this.imagemPrincipal = imagem
   }
+
+  produtos: any;
+  produtosRelacionados(categoria: any) {
+    this.produtoService.getProdutoCategoria(categoria).subscribe((res: any) => {
+      const produtos = res.produtos.filter((element: any) => element.id !== this.produto.id);
+  
+      this.produtos = produtos.map((element: any) => {
+        return {
+          ...element,
+          miniatura: this._sanitizer.bypassSecurityTrustResourceUrl('data:image/jpg;base64,' + element.miniatura)
+        };
+      });
+  
+      console.log(this.produtos);
+    });
+  }
+
+
+  maiorArmazenamento(produtos: any[]): string {
+    let maior = 0;
+    produtos.forEach((produto) => {
+      produto.armazenamento.forEach((armazenamento: any) => {
+        const gb = parseInt(armazenamento.replace('GB', ''));
+        if (gb > maior) {
+          maior = gb;
+        }
+      });
+    });
+    return `${maior}GB`;
+  }
+
+
+  formatarParcelado(preco: any): string {
+    // Remove todos os caracteres não numéricos
+    let precoNumerico = preco.replace(/\D/g, "");
+
+    // Adiciona a vírgula como separador decimal
+    precoNumerico = precoNumerico.replace(/(\d)(\d{2})$/, "$1,$2");
+
+    // Calcula o valor da parcela
+    let parcelado = parseFloat(precoNumerico) / 10;
+
+    // Formata o valor da parcela
+    return parcelado.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+}
+
+redirecionarParaOproduto(id: any) {
+  localStorage.setItem('idProduto', id);
+  this.router.navigate(['/produto']);
+}
 }
